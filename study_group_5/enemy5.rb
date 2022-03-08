7d0c86f04146b24e16ccee0b6632bd980e81d6c7
アダチユキコ <89229959+inco-yucco@users.noreply.github.com>
2022-03-08 16:47:05 +0900

    障害物複製

---
+++ study_group_5/enemy5.rb
@@ -0,0 +1,20 @@
+class Enemy5  # yellowの障害物
+  attr_accessor :mesh
+
+  def initialize(x, y, z, renderer, scene)
+    @mesh = Mittsu::Mesh.new(
+      Mittsu::BoxGeometry.new(5.0, 1.0, 1.0),
+      Mittsu::MeshBasicMaterial.new(color: 0xFFD500) # yellow
+    )
+    @mesh.position.set(x, y, z)
+
+    @renderer = renderer
+    @scene = scene
+  end
+
+  def update
+    # @mesh.rotation.x += 0.1
+    # @mesh.rotation.y += 0.1
+    # @mesh.position.z += 0.01
+  end
+end
